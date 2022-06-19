60ccc6c3b5e239d830bc1c68cb70026181e8b1ef
sena <hargiantofebrisena@gmail.com>
2022-06-19 15:36:25 +0700

    initial

--- Basic Data structure/Dict.py
+++ Basic Data structure/Dict.py
@@ -1,14 +1,19 @@
 tel = {'jack': 4098, 'sape': 4139}
+# tambah data e dict
 tel['guido'] = 4127
 print(tel)
 {'sape': 4139, 'guido': 4127, 'jack': 4098}
+# ganti data dict jack
 tel['jack'] = 4099
 print(tel)
+# delete data dict sape
 del tel['sape']
 tel['irv'] = 4127
 print(tel)
+# rubah dict ke list
 {'guido': 4127, 'irv': 4127, 'jack': 4098}
 print(list(tel.keys()))
+# rubah dict ke list dengan sorted
 ['irv', 'guido', 'jack']
 print(sorted(tel.keys()))
 print('guido' in tel)
--- Basic Data structure/Set.py
+++ Basic Data structure/Set.py
@@ -1,4 +1,6 @@
+# ambil unique dari suatu string
 data = set('abracadabraaaaa')
 print(data)
+# sama dengan ini
 data1 = {x for x in 'abracadabra' if x not in ''}
 print(data1)
